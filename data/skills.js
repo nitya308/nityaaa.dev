626cf541d19cae34c40d47b7563803060a4ce614
nitya-cs <nitya.agarwala@gmail.com>
2022-12-02 14:00:06 +0530

    pulls data from skills.js

--- data/skills.js
+++ data/skills.js
@@ -1,2 +1,28 @@
-export const skills = ["Java", "Kotlin", "Python", "JavaScript", "Typescript", "C", "ReactJS","NextJS", "Redux", "React-Native", "MongoDB", "Cloud Firestore",
+export const skills = ["Java", "Kotlin", "Python", "JavaScript", "Typescript", "C", "ReactJS", "NextJS", "Redux", "React-Native", "MongoDB", "Cloud Firestore",
   "Firebase", " NodeJS", "ExpressJS", "HTML/JSX", "CSS", "Git", "Bash", "Swift"]
+
+const ec1 = {
+  key: 0,
+  title: 'Teaching Assistant',
+  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
+  img: '/images/profile.jpg',
+  color: "#ffffff"
+}
+
+const ec2 = {
+  key: 1,
+  title: 'Teaching Assistant',
+  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
+  img: '/images/profile.jpg',
+  color: "#ffffff"
+}
+
+const ec3 = {
+  key: 2,
+  title: 'Teaching Assistant',
+  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
+  img: '/images/profile.jpg',
+  color: "#ffffff"
+}
+
+export const ecs = [ec1, ec2, ec3]
