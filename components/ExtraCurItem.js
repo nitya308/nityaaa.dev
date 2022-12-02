2355b07d0b602aa9ab28fd9a1b18f63c850d053b
nitya-cs <nitya.agarwala@gmail.com>
2022-12-02 13:57:47 +0530

    finished extracurriculars

--- components/ExtraCurItem.js
+++ components/ExtraCurItem.js
@@ -2,9 +2,10 @@ import styles from '../styles/extracurriculars.module.css';
 
 export default function ExtraCurItem({ item }) {
   return (
-    <div className={styles.item}>
-      
-
+    <div className={styles.item} style={{ backgroundColor: item.color }}>
+      <img src={item.img} className={styles.itemImage}></img>
+      <div className={styles.itemTitle}>{item.title}</div>
+      <div className={styles.itemDescription}>{item.description}</div>
     </ div>
   );
 }
--- components/Extracurriculars.js
+++ components/Extracurriculars.js
@@ -1,12 +1,28 @@
 import styles from '../styles/extracurriculars.module.css';
+import ExtraCurItem from './ExtraCurItem';
+
+const item = {
+  title: 'Teaching Assistant',
+  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
+  img: '/images/profile.jpg',
+  color: "#ffffff"
+}
+
+const item2 = {
+  title: 'Teaching Assistant',
+  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
+  img: '/images/profile.jpg',
+  color: "#ffffff"
+}
 
 export default function Extracurriculars() {
   return (
     <div className={styles.container}>
       <h3 className={styles.heading}>Work I Enjoy</h3>
       <div className={styles.extracurriculars}>
-        
-
+        <ExtraCurItem item={item}></ExtraCurItem>
+        <ExtraCurItem item={item2}></ExtraCurItem>
+        <ExtraCurItem item={item2}></ExtraCurItem>
       </div>
     </div>
   );
