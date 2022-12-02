aa4f543814c9463204bfd35465ff09a08ca41ffa
nitya-cs <nitya.agarwala@gmail.com>
2022-12-02 15:04:56 +0530

    updated skills

--- data/skills.js
+++ data/skills.js
@@ -4,25 +4,26 @@ export const skills = ["Java", "Kotlin", "Python", "JavaScript", "Typescript", "
 const ec1 = {
   key: 0,
   title: 'Teaching Assistant',
-  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
-  img: '/images/profile.jpg',
+  description: 'I am a TA and Section Leader for CS10 (Problem Solving using Programming) and now CS30 (Discrete Math) at Dartmouth. I love mentoring studnets and helping them learn and the spark when they first understand something',
+  img: '/images/dpine.png',
   color: "#ffffff"
 }
 
 const ec2 = {
   key: 1,
-  title: 'Teaching Assistant',
-  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
-  img: '/images/profile.jpg',
+  title: 'SEED',
+  description: 'I work at the Studio for Emergent Education Design which creates human centered design materials. I\'ve worked on projects on reframing assumptions and a guidebook to help engineering faculty integrate design projects into their courses.',
+  img: '/images/SEED.jpg',
   color: "#ffffff"
 }
 
 const ec3 = {
   key: 2,
-  title: 'Teaching Assistant',
-  description: 'I am a first year student at Dartmouth College, where I am pursuing a Bachelors in Computer Science and a minor in Human-Centered Design and Mathematics. I am also a member of the',
-  img: '/images/profile.jpg',
+  title: 'Oil Paint',
+  description: 'I\'ve loved painting forever and oil paint is the medium where I express myself the best! I\'m currently finding inspiration in: Dartmouth Fall🎨',
+  img: '/images/oilpainting.jpg',
   color: "#ffffff"
+
 }
 
 export const ecs = [ec1, ec2, ec3]
