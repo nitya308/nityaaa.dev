2974082384c886a433e2686918ec4b3ab32c5476
nitya-cs <nitya.agarwala@gmail.com>
2024-03-11 17:28:07 -0500

    updated some dev packages

--- components/Introduction.js
+++ components/Introduction.js
@@ -18,22 +18,19 @@ const renderSocialLinks = () => (
 
 export default function Introduction() {
   return (
-    <div className={styles.container}>
-      {/* <div className={styles.imageContainer}>
-        <img
-          src="/images/profile.jpg"
-          alt="some pictures of me"
-          width={300}
-          style={{ borderRadius: '5px' }}>
-        </img>
-      </div> */}
-      {/* <div className={styles.introContainer}> */}
-      <div className={styles.introContainer}>
-        <h1 className={styles.heading}>Hi! I'm Nitya</h1>
-        {/* <h2>Developer, Dartmouth College 🌲</h2> */}
-        A Computer Science and Human-Centered Design student at Dartmouth College 🌲 passionate about designing and building delightful experiences
+    <>
+      <div className={styles.container}>
+        <div className={styles.introContainer}>
+          <h1 className={styles.heading}>Hi! I'm Nitya</h1>
+          A Computer Science and Human-Centered Design student at Dartmouth College 🌲 passionate about designing and building delightful experiences
+        </div>
       </div>
-    </div>
-    // </div>
+      <div className={styles.introContainer3}>
+        These are the two halves of my brain:
+      </div>
+      <div className={styles.introContainer3} style={{ fontSize: 25, paddingBottom:10 }}>
+        Design & Coding
+      </div>
+    </>
   );
 }
